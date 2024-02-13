7ec79684d581b198c103b3c19adb07fc2c79f60a
Baxolise-lingani <you@example.com>
2024-02-13 16:38:59 +0200

    editted About

---
+++ src/components/AboutPage/About.jsx
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <div>
+        <section class="about" id="about">
+            
+            <div class="about-content">
+                <h2 class="heading">About <span>Me</span></h2>
+                <h3> Junior Software Developer</h3>
+                <p>I'm Baxolise, a passionate Software Developer with a background in Mathematical Science. My journey into the world of software development began with a deep curiosity about how mathematical principles and problem-solving skills could be applied to create innovative solutions in the digital realm.</p>
+
+                <p>I thrive on challenges and believe that every problem can be solved with the right approach and a dash of creativity. My mathematical modeling and data analysis skills have not only equipped me with a strong analytical mindset but also honed my ability to dissect complex problems and derive elegant solutions.</p>
+      
+                <p>As a Junior Software Developer, I'm committed to continuous learning and improvement. I have a solid foundation in HTML, CSS, and JavaScript, and I'm constantly expanding my knowledge by exploring front-end and back-end technologies. I'm excited to collaborate on projects that require a holistic understanding of web development and a touch of mathematical precision.</p>
+      
+                <p>Let's connect and explore the endless possibilities of combining mathematical science with the art of Software Developer!</p> 
+                <a href="#" class="btn">Learn More</a> 
+            </div>
+            <div class="about-img">
+                <img src="/src/assets/images/About.jpg" alt=""/>
+            </div>
+
+        </section>
+    </div>
+  )
+}
