e3ceca54d633a0285ecefe4da20f7d0e9cd1713e
Baxolise-lingani <princelingani@gmail.com>
2024-02-14 22:54:44 +0200

    modifying code

--- src/components/AboutPage/About.jsx
+++ src/components/AboutPage/About.jsx
@@ -1,14 +1,14 @@
 export default function About() {
   return (
     <main>
-      <section class="about" id="about">
-        <div class="about-img">
+      <section className="about" id="about">
+        <div className="about-img">
           <img src="/src/assets/images/About.jpg" alt=""/>
         </div>
         <div className="heading-container">
-          <h2 class="heading">About <span>Me</span></h2>
+          <h2 className="heading">About <span>Me</span></h2>
         </div>
-        <div class="about-content">
+        <div className="about-content">
         
           <h3> Junior Software Developer</h3>
           <p>I'm Baxolise, a passionate Software Developer with a background in Mathematical Science. My journey into the world of software development began with a deep curiosity about how mathematical principles and problem-solving skills could be applied to create innovative solutions in the digital realm.</p>
@@ -18,7 +18,7 @@ export default function About() {
           {/* <p>As a Junior Software Developer, I'm committed to continuous learning and improvement. I have a solid foundation in HTML, CSS, and JavaScript, and I'm constantly expanding my knowledge by exploring front-end and back-end technologies. I'm excited to collaborate on projects that require a holistic understanding of web development and a touch of mathematical precision.</p>
   
           <p>Let's connect and explore the endless possibilities of combining mathematical science with the art of Software Developer!</p>  */}
-          <a href="#" class="btn">Read More</a>
+          <a href="#" className="btn">Read More</a>
         </div>
 
       </section>
