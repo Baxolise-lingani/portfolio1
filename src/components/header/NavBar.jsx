77375a41ecf09a402f336293918652f3660c65c7
Baxolise-lingani <you@example.com>
2024-02-14 16:05:56 +0200

    logo is clickable

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -3,31 +3,28 @@ export default function NavBar() {
   return (
     <div>
         {/* <!-- Header --> */}
-<header>
-    {/* <!-- Navigation --> */}
-    <nav>
-        <div id="left-links">
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#skills">Skills</a>
-            <a href="#work-experience">Work Experience</a>
-            <a href="#portfolio">Portfolio</a>
-        </div>
+        <header>
+            {/* <!-- Navigation --> */}
+            <nav>
+                <div id="left-links">
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#work-experience">Work Experience</a>
+                    <a href="#portfolio">Portfolio</a>
+                </div>
 
-        <div id="logo">
-            <a href="#">Baxolise Lingani</a>
-        </div>
+                <div id="logo">
+                    <a href="#home">Baxolise Lingani</a>
+                </div>
 
-        <div id="contact-links">
-            <a href="#message">Message</a>
-            <a href="#call">Call Me</a>
-
-             
-
-        </div>
-    </nav>
-    <i class='bx bx-menu' id="menu-icon"></i>
-</header>
+                <div id="contact-links">
+                    <a href="#message">Message</a>
+                    <a href="#call">Call Me</a>
+                </div>
+            </nav>
+            <i class='bx bx-menu' id="menu-icon"></i>
+        </header>
     </div>
   )
 }
