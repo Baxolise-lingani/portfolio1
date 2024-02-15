4c88e9cd5706d484aea9999f11f47c6cb4f67ad0
Baxolise-lingani <you@example.com>
2024-02-15 16:23:36 +0200

    Experience CSS

---
+++ src/components/WorkExperience page/Experience.jsx
@@ -0,0 +1,40 @@
+export default function Experience() {
+  return (
+    <>
+
+    <section className="experience" id="experience">
+        <div className="main">
+            <h3 className="head">Work Experience</h3>
+            <div className="container">
+                <ul>
+                    <li>
+                        <h3 className="heading">Junior Data Analyst</h3>
+                        <p>Start: 2021-06 </p><br/>
+                        <p>End  : 2021-12 </p>
+                        <a href="#">Read More </a>
+                        <span className="company">CPUT Library</span>
+                        <span className="circle"></span>
+                    </li>
+                    <li>
+                        <h3 className="heading">Software Developer Intern</h3>
+                        <p>Start: 2023-07 </p><br/>
+                        <p>End  : 2024-01 </p>
+                        <a href="#">Read More </a>
+                        <span className="company">Uvu Africa</span>
+                        <span class="circle"></span>
+                    </li>
+                    <li>
+                        <h3 className="heading">Junior Software Developer</h3>
+                        <p>Start: 2024-01 </p><br/>
+                        <p>End  : Present </p>
+                        <a href="#">Read More </a>
+                        <span className="company">Younglings</span>
+                        <span className="circle"></span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
