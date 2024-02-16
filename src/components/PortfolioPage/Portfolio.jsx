c9b97d513da843285dec1e234ddf6197691525ef
Baxolise-lingani <you@example.com>
2024-02-16 15:09:48 +0200

    modifying pictures

--- src/components/PortfolioPage/Portfolio.jsx
+++ src/components/PortfolioPage/Portfolio.jsx
@@ -1,32 +1,44 @@
 // TODO: focusing on Portfolio section
-function Portfolio() {
+export default function Portfolio() {
   return (
     <>
-     <section class="portfolio">
-        <h3 class="head">Portfolio</h3>
-        <div class="portfolio-nav">
+     <section className="portfolio" id="portfolio">
+        <h3 className="head">Previous <span>Project</span></h3>
+        <div className="portfolio-nav">
             <a href="#">All</a>
             <a href="#">Website</a>
             <a href="#">Application</a>
             <a href="#">Personal Website</a>
         </div>
-        <div class="project-box">
-            <img src="project1.jpg" alt="Project 1" class="project-image"/>
-            <div class="project-details">
-                <div class="project-title">Project 1</div>
-                <div class="project-links">
-                    <a href="#">Live Demo</a>
-                    <a href="#">GitHub</a>
+        <div className="project-container">
+            <div className="project-box">
+                <img src="/src/assets/images/bakery.jpg" alt="Project 1" className="project-image"/>
+                <div className="project-details">
+                    <div className="project-title">Project 1</div>
+                    <div className="project-links">
+                        <a href="#">Live Demo</a>
+                        <a href="#">GitHub</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="project-box">
-            <img src="project2.jpg" alt="Project 2" class="project-image"/>
-            <div class="project-details">
-                <div class="project-title">Project 2</div>
-                <div class="project-links">
-                    <a href="#">Live Demo</a>
-                    <a href="#">GitHub</a>
+            <div className="project-box">
+                <img src="/src/assets/images/bank app.png" alt="Project 2" className="project-image"/>
+                <div className="project-details">
+                    <div className="project-title">Project 2</div>
+                    <div className="project-links">
+                        <a href="#">Live Demo</a>
+                        <a href="#">GitHub</a>
+                    </div>
+                </div>
+            </div>
+            <div className="project-box">
+                <img src="/src/assets/images/Personal website.jpg" alt="Project 3" className="project-image"/>
+                <div className="project-details">
+                    <div className="project-title">Project 3</div>
+                    <div className="project-links">
+                        <a href="#">Live Demo</a>
+                        <a href="#">GitHub</a>
+                    </div>
                 </div>
             </div>
         </div>
