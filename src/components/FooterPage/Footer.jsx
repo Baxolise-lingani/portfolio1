0c35a2f0e45dc9f4f001395712207197866909d5
Baxolise-lingani <you@example.com>
2024-02-19 16:49:32 +0200

    added footer component

---
+++ src/components/FooterPage/Footer.jsx
@@ -0,0 +1,39 @@
+export default function Footer() {
+  return (
+    <>
+        <section className="call" id="call">
+
+            <div className="box-container">
+
+                <div className="box">
+                    <h3>Baxolise Lingani</h3>
+                    <p>Crafting Code, Shaping Tomorrow: Empowering the Future,<br/>
+                        One Line at a Time.</p>
+                </div>
+
+                <div className="box">
+                    <h3> quick links</h3>
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#experience">Work Experience</a>
+                    <a href="#portfolio">Portfolio</a>
+                </div>
+
+                <div className="box">
+                    <h3>contact Me</h3>
+                    <a href="#"><i className='bx bxl-whatsapp'></i></a>
+                    <a href="#"><i className='bx bxl-instagram' ></i></a>
+                    <a href="#"><i className='bx bxl-linkedin' ></i></a>
+                    <a href="#"><i className='bx bxl-github' ></i></a>
+                </div>
+
+            </div>
+
+            <div class="credit"><i class='bx bx-copyright' ></i> by <span>Baxolise Lingani</span> | at Younglings Africa </div>
+        </section>
+    
+    
+    </>
+  )
+}
