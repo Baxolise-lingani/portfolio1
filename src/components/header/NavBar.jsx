74b19ffd71317783fe6d0fbd63c7bbf31b1985bf
Baxolise-lingani <you@example.com>
2024-02-23 16:31:59 +0200

    fixing harmburg

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -8,42 +8,43 @@ export default function NavBar() {
   };
 
   return (
-    <div className="navbar-wrapper">
-      <header className={`header ${isMobileMenuOpen ? 'sticky' : ''}`}>
-        {/* Navigation */}
-        <nav>
-          <div id="left-links" className={isMobileMenuOpen ? 'mobile-hidden' : ''}>
+    <div>
+        {/* <!-- Header --> */}
+        <header className={isMobileMenuOpen ? 'mobile-menu-open' : ''}>
+            {/* <!-- Navigation --> */}
+            <nav>
+                <div id="left-links" className={isMobileMenuOpen ? 'mobile-hidden' : ''}>
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#experience">Work Experience</a>
+                    <a href="#portfolio">Portfolio</a>
+                </div>
+
+                <div id="logo">
+                    <a href="#home">Baxolise Lingani</a>
+                </div>
+
+                <div id="contact-links">
+                    <a href="#contact">Message</a>
+                    <a href="#call">Call Me</a>
+                </div>
+
+                <i className='bx bx-menu' id="menu-icon" onClick={toggleMobileMenu}></i>
+            </nav>
+          
+
+            {/* Mobile Menu */}
+          <div className={`mobile-menu ${isMobileMenuOpen ? 'show' : ''}`}>
             <a href="#home">Home</a>
             <a href="#about">About</a>
             <a href="#skills">Skills</a>
             <a href="#experience">Work Experience</a>
             <a href="#portfolio">Portfolio</a>
-          </div>
-
-          <div id="logo">
-            <a href="#home">Baxolise Lingani</a>
-          </div>
-
-          <div id="contact-links" className={isMobileMenuOpen ? 'mobile-hidden' : ''}>
             <a href="#contact">Message</a>
             <a href="#call">Call Me</a>
           </div>
-
-          {/* Mobile Menu Icon */}
-          <i className="bx bx-menu" id="menu-icon" onClick={toggleMobileMenu}></i>
-        </nav>
-
-        {/* Mobile Menu */}
-        <div className={`mobile-menu ${isMobileMenuOpen ? 'show' : ''}`}>
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#skills">Skills</a>
-          <a href="#experience">Work Experience</a>
-          <a href="#portfolio">Portfolio</a>
-          <a href="#contact">Message</a>
-          <a href="#call">Call Me</a>
-        </div>
-      </header>
+        </header>
     </div>
-  );
+  )
 }
