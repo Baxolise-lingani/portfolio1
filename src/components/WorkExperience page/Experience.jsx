5e5f98aa394c321e5082cdba0f8fd0aef7fbf413
Baxolise-lingani <you@example.com>
2024-02-23 16:32:49 +0200

    fixing h3

--- src/components/WorkExperience page/Experience.jsx
+++ src/components/WorkExperience page/Experience.jsx
@@ -3,14 +3,16 @@ export default function Experience() {
     <>
     <section className="experience" id="experience">
         <div className="main">
-            <h3 className="head">Work Experience</h3>
+            <div className="head">
+                <h3>Work Experience</h3>
+            </div>
             <div className="container-all">
                 <ul>
                     <li>
                         <h3 className="heading">Junior Data Analyst</h3>
                         <p>Start: 2021-06 </p><br/>
                         <p>End  : 2021-12 </p>
-                        <a href="#">Read More </a>
+                        {/* <a href="#">Read More </a> */}
                         <span className="company">CPUT Library</span>
                         <span className="circle"></span>
                     </li>
@@ -18,7 +20,7 @@ export default function Experience() {
                         <h3 className="heading">Software Developer Intern</h3>
                         <p>Start: 2023-07 </p><br/>
                         <p>End  : 2024-01 </p>
-                        <a href="#">Read More </a>
+                        {/* <a href="#">Read More </a> */}
                         <span className="company">Uvu Africa</span>
                         <span class="circle"></span>
                     </li>
@@ -26,7 +28,7 @@ export default function Experience() {
                         <h3 className="heading">Junior Software Developer</h3>
                         <p>Start: 2024-01 </p><br/>
                         <p>End  : Present </p>
-                        <a href="#">Read More </a>
+                        {/* <a href="#">Read More </a> */}
                         <span className="company">Younglings</span>
                         <span className="circle"></span>
                     </li>
